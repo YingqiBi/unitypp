b75252510ee39804808e7b60e7ba14c1a151abe9
YingqiBi <116724456+YingqiBi@users.noreply.github.com>
2022-11-21 08:53:00 +0100

    03

---
+++ case6.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class case6 : MonoBehaviour
+{
+    public float movespeed = 5f;
+    public Vector3 moveDirecton;
+
+    // Update is called once per frame
+    public void Update()
+    {
+        Keyboard currentKeyboard = Keyboard.current;
+        if (currentKeyboard.bKey.isPressed)
+        {
+            moveDirecton = Vector3.right;
+        }
+        else if (Input.GetKey("a"))
+        {
+            moveDirecton = Vector3.left;
+        }
+        else
+        {
+            moveDirecton = Vector3.zero;
+        }
+    }
+    public void FixedUpdate()
+    {
+        Vector3 movement = moveDirecton * Time.fixedDeltaTime * movespeed;
+        transform.position += movement;
+    }
+}
